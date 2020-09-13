0a6e5425924fca9bca92da323314df9660825c91
RodrigoPegasano <rodrigo.pegasano@gmail.com>
2020-09-13 15:15:43 -0300

    Pastas atualizadas e material Node novo add

---
+++ Conteudo/Node/1.arquiteturaClienteServidor.js
@@ -0,0 +1,36 @@
+// Arquitetura Cliente-Servidor (Client-side)
+// No desenvolvimento web, esta arquitetura refere-se a um modelo de comunicação que liga varios dispositivos a um servidor na internet
+
+// O que chamamos de cliente?
+// Estes são os dispositivos que fazem pedidos de serviços ou recursos a um servidor
+// Ex: um computador, um celular, um tablet, um console videogame, etc.
+// Dentro da internet, o cliente geralmente acessa esses serviços e recursos através de um navegador web
+
+// O que chamamos de servidor?
+// É o computador que fornece os serviços e recursos aos quais os clientes tem acesso
+// É muito comum que o mesmo computador seja o servidor e o cliente no desenvolvimento web e de apps
+
+// Fluxo cliente servidor:
+
+// Request/Aplicação
+// É a requisição que realizamos a través do browser (cliente) paraum servidor
+// Ex: O bowser faz o request a página do Facebook que está armazenada e um servidor
+
+//  Response/Resposta
+// O servidor recebe o nosso request, o processa e envia como resultado uma response ao cliente
+// Ex: O servidor retorna o cliente á página inicial do Facebook
+
+// Por que é importante conhecer esse fluxo de Request-Response?
+// Dentro dentro do mundo do desenvolvimento web, as aplicações tem duas 'pontas':
+//     - Frontend e Backend
+
+// Frontend:
+// É tudo que acontece do lado do cliente (browser). Inclui todos os elementos gráficos que compõem a interfase do site
+// Linguagens utilizadas: HTML (estrutura), CSS (estilos visuais), JavaScript (interação/programação dentro do site)
+
+// Backend:
+// É tudo que acontece do lado do servidor. O que permite que todos os requests solicitados pelo cliente sejam processados e respondidos
+// Linguagens utilizadas: MySQL (banco de dados), PHP (sites dinâmicos)
+
+// OBSERVAÇÃO:
+// Hoje existe a possibilidade de utilizar o JavaScript tanto no back como no front, tornando o desenvolvimento muito mais fluido

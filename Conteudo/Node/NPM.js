b003aca5771809f7ecf18bb876502b926a34eeb0
RodrigoPegasano <rodrigo.pegasano@gmail.com>
2020-09-13 15:55:18 -0300

    Adicionando NPM

---
+++ Conteudo/Node/NPM.js
@@ -0,0 +1,10 @@
+// NPM (Node Package Manager = Gerenciador de Pacotes Node)
+
+// É uma grande biblioteca de soluções para o node
+
+// 1. Precisamos criar uma pasta no nosso computador
+// 2. Entrar nela no terminal e assim que estiver nela colocar 'npm init'
+//    Esse npm init mostrará todas as informações da pasta e você terá que confirmar se esta tudo certo
+//    Caso queira pular esse passo de confirmação, colocar 'npm init -y'
+// 3. Após a confirmação, o node criar na pasta um arquivo 'package.json' que mostra todas as infomrações dessa pasta
+// A partir daquí a gente começa a baixar bibliotecas para nossa pasta

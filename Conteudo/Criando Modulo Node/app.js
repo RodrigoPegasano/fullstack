efe7c10d3728e9e8e61d1c85218353a567e9d4c8
RodrigoPegasano <rodrigo.pegasano@gmail.com>
2020-09-14 09:25:17 -0300

    Pasta nova Criando Modulo

---
+++ Conteudo/Criando Modulo Node/app.js
@@ -0,0 +1,25 @@
+// Vamos fazer funcionar um modulo node 'file system' (fs)
+
+// MÓDULO NATIVO
+
+// require()
+// O require chama uma pasta que você quer acessar no seu computador, uma file system
+const fs = require('fs');
+
+let dados = fs.readFileSync(__dirname + '/dados.txt', 'utf8');
+
+console.log(dados);
+// O readFileSync vai ler o as pastas que você mandar ele pesquisar
+// No () colocamos __dirname que analiza todas as pastas no seu computador e depois colocamos como complemento '/dados.txt' pois é o nosso
+// destino final
+// O 'utf8' para ele saber as cadeias de caracteres
+
+
+// MÓDULO INSTALADO
+const moment = require('moment');
+
+let data = moment().format('MMM-YYYY');
+
+console.log(data);
+// moment é a biblioteca instalada
+// format para formatar e a gente esta pedindo o mês e ano separados por um traço

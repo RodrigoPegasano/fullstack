86432d166d96f1b9eeb58bd9f7732a0a0ad3dfd1
RodrigoPegasano <rodrigo.pegasano@gmail.com>
2020-09-17 08:20:20 -0300

    Atualizando material Node

---
+++ Conteudo/Node/4.introducaoHTTP.js
@@ -0,0 +1,23 @@
+// HTTP = Hyper Text Transfer Protocol
+// URI = Uniform Resource Identificator
+
+// HTTP: É o protocolo que administra as transações web entre os clientes e os servidores
+
+// Métodos mais utilizados pelos pelo HTTP:
+
+// GET (solicitar dados):
+// É utilizado para solicitar informações do servidor de um recurso específico. Toda vez que digitamos um endereço no navegador
+// ou acessamos um link, estamos usando o método GET. Se você quiser enviar informações para o servidor usando este método, ele irá
+// viajar através da URL.
+
+// POST (envia dados. muito seguro):
+// É usado para enviar dados para o servidor. Éste método é mais seguro do que o get, uma vez que a informação não viaja através do URL.
+
+// PUT (Envie dados, substitua todas as informações de um recurso):
+// É muito parecido com o post. É usado para substituir todas as informações atuais de um recurso presente no servidor.
+
+// PATCH (Envia dados, modifica informação parcial de um recurso):
+// Similar ao PUT. É utilizado para ampliar modificações parciais a um recurso no servidor.
+
+// DELETE (Envia uma solicitação para eliminar um recurso do servidor):
+// Elimina um recurso presente no servidor. Quando apagamos uma publicação no Facebook, por exemplo, estamos utilizando este método.

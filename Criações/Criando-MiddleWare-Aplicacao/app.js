71fb5fd95d8a1fc2af8ff110a934780fa23740f3
Pegasano <lucio.pegasano@capgemini.com>
2020-11-03 19:09:36 -0300

    Adicionando Express II

--- Criações/Criando-MiddleWare-Aplicacao/app.js
+++ Criações/Criando-MiddleWare-Aplicacao/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var logMiddleware = require('./middlewares/logSite');
+var logMiddleware = require('./middlewares/logSite');''
 
 var app = express();
 
